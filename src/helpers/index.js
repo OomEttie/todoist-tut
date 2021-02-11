051a8b3da7d4210de4c1fb12d2d57428dd5d953c
Etienne Landman <oomettie@gmail.com>
2021-02-11 16:20:23 +0200

    wip

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,8 @@
 import { collatedTasks } from '../constants'
 
+export const getTitle = (projects, projectid) =>
+  projects.find((project) => project.projectid === projectid)
+
 export const collatedTasksExist = (selectedProject) => {
   collatedTasks.find((task) => task.key === selectedProject)
 }
